2850de48538b437b10c3c566b70a509399a12240
ishma2754 <ishmakhan1995@gmail.com>
2024-02-26 12:23:23 +0530

    generate html for the order summary

--- data/cart.js
+++ data/cart.js
@@ -1,6 +1,13 @@
 //PART E
 //Add products to this cart
-export const cart = []; //export the variable that you want to take out
+export const cart = [{
+  productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+  quantity: 2,
+},{
+  productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+  quantity: 1
+}];// no need to save data twice inside products array and cart array we can search through product id ===> de-duplicating data/normalizing the data
+ //export the variable that you want to take out
 
 export function addToCart (productId) {
   let matchingItem;
