06896d47cbb9bd653fabd3816309cecc87fc649d
ishma2754 <ishmakhan1995@gmail.com>
2024-03-08 13:50:51 +0530

    save data and generate html for delivery option

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,22 @@
 import {cart, removeFromCart, calculateCartQuantity, updateQuantity} from '../data/cart.js';
 import {products} from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
+import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js'; //write url of esm
+import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';//default export only when importing 1 thing // import the file without curly brackets but only when importing one file NAMED EXPORT
+import {deliveryOptions} from '../data/deliveryOptions.js';
+
+
+hello();
+
+dayjs(); //all lower case
+
+const today = dayjs(); //fetch current date
+const deliveryDate = today.add(7,'days'); //add two parameters add 7 days to today's date
+console.log(deliveryDate.format('dddd, MMMM D'));
+
+
+
+
 
 //added matchingId to radio selectors 
 //single dot slash means same folder
@@ -22,11 +38,33 @@ cart.forEach((cartItem) => {
     }
   });
 
+
+  const deliveryOptionId = cartItem.deliveryOptionId;
+
+  let deliveryOption;
+
+  deliveryOptions.forEach((option) => {
+    if (option.id === deliveryOptionId) {
+      deliveryOption = option;
+    }
+  });
+
+  const today = dayjs(); //to call today's date
+  const deliveryDate = today.add(
+    deliveryOption.deliveryDays,
+    'days'
+  );
+
+  const dateString = deliveryDate.format(
+    'dddd, MMMM D'
+  );
+
+
   cartSummaryHTML += `
     <div class="cart-item-container 
     js-cart-item-container-${matchingProduct.id}">
     <div class="delivery-date">
-      Delivery date: Tuesday, June 21
+      Delivery date: ${dateString}
     </div>
 
     <div class="cart-item-details-grid">
@@ -59,50 +97,50 @@ cart.forEach((cartItem) => {
         <div class="delivery-options-title">
           Choose a delivery option:
         </div>
-        <div class="delivery-option">
-          <input type="radio" checked
-            class="delivery-option-input"
-            name="delivery-option-${matchingProduct.id}">
-          <div>
-            <div class="delivery-option-date">
-              Tuesday, June 21
-            </div>
-            <div class="delivery-option-price">
-              FREE Shipping
-            </div>
-          </div>
-        </div>
-        <div class="delivery-option">
-          <input type="radio"
-            class="delivery-option-input"
-            name="delivery-option-${matchingProduct.id}">
-          <div>
-            <div class="delivery-option-date">
-              Wednesday, June 15
-            </div>
-            <div class="delivery-option-price">
-              $4.99 - Shipping
-            </div>
-          </div>
-        </div>
-        <div class="delivery-option">
-          <input type="radio"
-            class="delivery-option-input"
-            name="delivery-option-${matchingProduct.id}">
-          <div>
-            <div class="delivery-option-date">
-              Monday, June 13
-            </div>
-            <div class="delivery-option-price">
-              $9.99 - Shipping
-            </div>
-          </div>
-        </div>
+        ${deliveryOptionsHTML(matchingProduct, cartItem)}
       </div>
     </div>
   </div>
  `;
 });
+
+function deliveryOptionsHTML (matchingProduct, cartItem) {
+
+  let html = '';
+  deliveryOptions.forEach((deliveryOption) => {
+    const today = dayjs(); //to call today's date
+    const deliveryDate = today.add(
+      deliveryOption.deliveryDays,
+      'days'
+    );
+
+    const dateString = deliveryDate.format(
+      'dddd, MMMM D'
+    );
+
+    const priceString = deliveryOption.priceCents === 0 ?  'FREE' : `$${formatCurrency(deliveryOption.priceCents)} -`;//if price is zero freeshipping otherwise ternary operator
+
+    const isChecked = deliveryOption.id === cartItem.deliveryOptionId; //we need to have blue tick when we load page//we don't have access to cartItem
+    html += ` 
+      <div class="delivery-option">
+        <input type="radio"
+          ${isChecked ? 'checked' : ''}
+          class="delivery-option-input"
+          name="delivery-option-${matchingProduct.id}">
+        <div>
+          <div class="delivery-option-date">
+            ${dateString}
+          </div>
+          <div class="delivery-option-price">
+           ${priceString} Shipping
+          </div>
+        </div>
+      </div>
+    `
+  });
+
+  return html;
+}
 
 document.querySelector('.js-order-summary')
  .innerHTML = cartSummaryHTML;
--- scripts/utils/money.js
+++ scripts/utils/money.js
@@ -3,3 +3,5 @@
 export function formatCurrency(priceCents){
   return (priceCents / 100).toFixed(2);
 }
+
+export default formatCurrency;// import the file without curly brackets but only when importing one file
